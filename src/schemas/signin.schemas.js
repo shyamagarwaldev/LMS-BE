7b3b557d89c5a7fe9911d89f84be508789ff8278
Shyam Agarwal <shyamagarwal3800@gmail.com>
2026-01-28 00:46:58 +0530

    fixing file name of singin and signup schema

--- src/schemas/signin.schemas.js
+++ src/schemas/signin.schemas.js
@@ -1,12 +1,8 @@
 import { z } from "zod";
 
-const signUpSchema = z.object({
-  username: z
-    .string()
-    .min(3, "The username is too short")
-    .max(50, "The username is too long"),
+const signInSchema = z.object({
   email: z.string().email("Invailed Email"),
   password: z.string().min(8, "The password must be altest 8 character"),
 });
 
-export default signUpSchema;
+export default signInSchema;
--- src/schemas/signup.schemas.js
+++ src/schemas/signup.schemas.js
@@ -1,8 +1,12 @@
 import { z } from "zod";
 
-const signInSchema = z.object({
+const signUpSchema = z.object({
+  username: z
+    .string()
+    .min(3, "The username is too short")
+    .max(50, "The username is too long"),
   email: z.string().email("Invailed Email"),
   password: z.string().min(8, "The password must be altest 8 character"),
 });
 
-export default signInSchema;
+export default signUpSchema;
