09d8ec709a7bc4f2099aa77a5d717f4a3ae0335b
Shyam Agarwal <shyamagarwal3800@gmail.com>
2026-03-20 15:59:39 +0530

    added start script

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import upload from "../middlewares/multer.middlewares.js";
+
 import Auth from "../middlewares/auth.middlewares.js";
 import {
   getAllAdminCourses,
